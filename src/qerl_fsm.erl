f7ca7c1f4b0df1558e3c243336f9682f153a08c3
alinpopa <alin.popa@gmail.com>
2010-07-27 22:42:03 +0300

    Use more relevant state names.

--- src/qerl_fsm.erl
+++ src/qerl_fsm.erl
@@ -3,28 +3,36 @@
 
 -export([start_link/0, set_socket/1]).
 -export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
--export([parsing/2, wait_for_socket/2]).
+
+% states
+-export(['INCOMPLETE_FRAME'/2, 'END_OF_FRAME'/2, 'WAIT_FOR_SOCKET'/2]).
 
 -import(qerl_protocol, [is_eof/1]).
 -import(qerl_utils, [append/2]).
 
-wait_for_socket({socket_ready, Socket}, _State) ->
+'WAIT_FOR_SOCKET'({socket_ready, Socket}, _State) ->
     inet:setopts(Socket, [{active, once}]),
-    {next_state, parsing, create_state_data(Socket,[])}.
+    {next_state, 'INCOMPLETE_FRAME', create_state_data(Socket,[])}.
 
-parsing(_, State) ->
-    {{socket,Socket},{data,Data}} = State,
+'INCOMPLETE_FRAME'({data, NewData}, State) ->
+    {{socket,Socket},{data,IncompleteData}} = State,
+    Data = append(IncompleteData, NewData),
     case is_eof(Data) of
         true ->
             info("done parsing data:~n~p~n",[Data]),
-            {next_state, parsing, create_state_data(Socket,[])};
+            {next_state, 'END_OF_FRAME', create_state_data(Socket,[])};
         _    ->
             info("incomplete data:~n~p~n",[Data]),
-            {next_state, parsing, create_state_data(Socket,Data)}
+            {next_state, 'INCOMPLETE_FRAME', create_state_data(Socket,Data)}
     end.
+
+'END_OF_FRAME'({data, NewData}, State) ->
+    {{socket, Socket}, {_, _}} = State,
+    {next_state, 'INCOMPLETE_FRAME', create_state_data(Socket, NewData)}.
 
 create_state_data(Socket,Data) -> {{socket,Socket},{data,Data}}.
 
+info(Msg) -> info(Msg,[]).
 info(Msg,Params) -> error_logger:info_msg(Msg,Params).
 
 %% gen_fsm functions
@@ -35,17 +43,15 @@ set_socket(Socket) ->
     gen_fsm:send_event(?MODULE, {socket_ready,Socket}).
 
 %% gen_fsm callbacks
-init([]) -> {ok, wait_for_socket, create_state_data(none,[])}.
+init([]) -> {ok, 'WAIT_FOR_SOCKET', create_state_data(none,[])}.
 
 handle_event(_Event, StateName, State) -> {next_state, StateName, State}.
 handle_sync_event(_Event, _From, StateName, State) -> {reply, ok, StateName, State}.
 
 handle_info({tcp, Socket, Bin}, StateName, State) ->
     inet:setopts(Socket, [{active, once}]),
-    {{socket,Socket},{data,IncompleteData}} = State,
     ListData = erlang:binary_to_list(Bin),
-    Data = append(IncompleteData, ListData),
-    ?MODULE:StateName({parse,Socket},create_state_data(Socket,Data));
+    ?MODULE:StateName({data,ListData},State);
 handle_info({tcp_closed, _Socket}, _StateName, State) -> {stop, normal, State};
 handle_info(_Info, StateName, State) -> {noreply, StateName, State}.
 
