ec809362b4dca1bcd94fb020695fdbb1fdc9d22c
alinpopa <alin.popa@gmail.com>
2010-07-30 08:38:10 +0300

    Add extra logging information

--- src/qerl_example_server.erl
+++ src/qerl_example_server.erl
@@ -1,6 +1,6 @@
 -module(qerl_example_server).
 -export([start/0,stop/0]).
 
-start() -> qerl_socket_server:start(?MODULE, 7000, {qerl_fsm, start_link}).
-stop() -> qerl_socket_server:stop(?MODULE).
+start() -> qerl_socket_server:start(7000).
+stop() -> qerl_socket_server:stop().
 
--- src/qerl_socket_server.erl
+++ src/qerl_socket_server.erl
@@ -3,25 +3,25 @@
 
 -export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 -export([accept_loop/1]).
--export([start/3, stop/1]).
+-export([start/1, stop/0]).
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
 -record(server_state, {
         port,
-        loop,
         ip=any,
         lsocket=null}).
 
-start(Name, Port, Loop) ->
+start(Port) ->
     io:format("Starting gen_server"),
-    State = #server_state{port = Port, loop = Loop},
-    gen_server:start_link({local, Name}, ?MODULE, State, []).
+    State = #server_state{port = Port},
+    gen_server:start_link({local, ?MODULE}, ?MODULE, State, []).
 
-stop(Name) ->
-    gen_server:call(Name, {stop}).
+stop() ->
+    gen_server:call(?MODULE, {stop}).
 
 init(State  = #server_state{port=Port}) ->
+    process_flag(trap_exit, true),
     case gen_tcp:listen(Port, ?TCP_OPTIONS) of
         {ok, LSocket} ->
             io:format("Listen using LSocket: ~p~n",[LSocket]),
@@ -31,33 +31,37 @@ init(State  = #server_state{port=Port}) ->
             {stop, Reason}
     end.
 
-handle_cast({accepted, {LSocket, _Pid}}, State=#server_state{lsocket = LSocket})  ->
+handle_cast({accepted, _Pid}, State=#server_state{})  ->
     {noreply, accept(State)}.
 
-accept_loop({Server, LSocket, {M, F}})  ->
+accept_loop({Server, LSocket})  ->
     {ok, Socket} = gen_tcp:accept(LSocket),
-    gen_server:cast(Server, {accepted, {LSocket, self()}}),
-    {ok,Pid} = qerl_fsm:start_link(),
-    gen_tcp:controlling_process(Socket, Pid),
-    qerl_fsm:set_socket(Socket).
+    gen_server:cast(Server, {accepted, self()}),
+    case qerl_fsm:start_link() of
+        {ok, Pid} ->
+            gen_tcp:controlling_process(Socket,Pid),
+            qerl_fsm:set_socket(Socket);
+        Else -> error_logger:error_msg("error starting qerl_fsm process",[Else])
+    end.
 
 % To be more robust we should be using spawn_link and trapping exits
-accept(State = #server_state{lsocket=LSocket, loop = Loop}) ->
-    io:format("accept~n"),
-    proc_lib:spawn(?MODULE, accept_loop, [{self(), LSocket, Loop}]),
+accept(State = #server_state{lsocket=LSocket}) ->
+    %proc_lib:spawn(?MODULE, accept_loop, [{self(), LSocket}]),
+    proc_lib:spawn_link(?MODULE, accept_loop, [{self(), LSocket}]),
     State.
 
 % These are just here to suppress warnings.
 handle_call({stop}, _Caller, State) ->
+    io:format(" handle_call ~p~n",[State]),
     {stop, ok, State};
 handle_call(_Msg, _Caller, State) -> 
     {noreply, State}.
-handle_info(_Msg, Library) -> {noreply, Library}.
+handle_info(Msg, Library) ->
+    io:format(" -> handle_info: Msg - ~p, Library - ~p~n",[Msg,Library]),
+    {noreply, Library}.
 terminate(_Reason, State) ->
-    LSocket = State#server_state.lsocket,
-    io:format("LSocket stop: ~p~n",[LSocket]),
-    CloseResponse = gen_tcp:close(LSocket),
-    io:format("Close response: ~p~n",[CloseResponse]),
+    io:format(" terminate ~p~n",[State]),
+    gen_tcp:close(State#server_state.lsocket),
 	ok.
 
 code_change(_OldVersion, Library, _Extra) -> {ok, Library}.
