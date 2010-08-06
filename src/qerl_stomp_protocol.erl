665c9c84dff3ea490ef93d8fcb2394509909902a
alinpopa <alin.popa@gmail.com>
2010-08-06 15:57:35 +0300

    Cleanup a bit stomp protocol parser. Add other stomp protocol SEND command.

--- src/qerl_stomp_protocol.erl
+++ src/qerl_stomp_protocol.erl
@@ -1,18 +1,12 @@
 -module(qerl_stomp_protocol).
--export([is_eof/1, parse_frame/1, parse/1]).
--import(string, [str/2]).
--import(lists, [splitwith/2, flatten/1]).
--import(re, [split/2]).
--import(qerl_utils, [strip_cr/1, trim/1, bin_to_list/1]).
+-export([is_eof/1, parse/1]).
+
+-import(binary, [bin_to_list/1]).
 
 -define(LF,10).
 -define(CR,13).
 -define(NULL,0).
-
-%% Check if the passed list contains an end of frame.
-%%%is_eof(ListData) ->
-%%%    End = str(strip_cr(ListData),[10,0]),
-%%%    End =/= 0.
+-define(COLLON,58).
 
 is_eof(Bin) ->
     case binary:match(drop_cr(Bin),<<?LF,?NULL>>) of
@@ -20,52 +14,56 @@ is_eof(Bin) ->
         _ -> true 
     end.
 
-%% Parse the given frame and return a structure having the format:
-%% {{command, []}, {headers, [[]]}, {body, []}}
-parse_frame(RawListData) ->
-    ListData = strip_cr(RawListData),
-    {Command, Rest} = splitwith(fun(X) -> X =/= 10 end, ListData),
-    [Headers|Body] = proc_rest(Rest),
-    {{command, Command},{headers, proc_headers(Headers)}, {body, proc_body(Body)}}.
-
-proc_rest(RestData) ->
-    Rest = split(RestData, [10,10]),
-    bin_to_list(Rest).
-
-proc_headers(Headers) ->
-    BinH = split(Headers, [10]),
-    bin_to_list(BinH).
-
-proc_body(Body) ->
-    trim(flatten(Body)).
-
 parse(Bin) ->
-    LfBin = drop_cr(Bin),
-    parse_msg(binary:split(LfBin,<<?LF>>,[global])). 
+    LfBin = drop_cr(get_valid_data(Bin)),
+    parse_msg(binary:split(LfBin,<<?LF>>,[trim])). 
 
-parse_msg([<<"CONNECT">>|T]) -> {connect, {headers,get_headers(T)}};
-parse_msg([<<"DISCONNECT">>|T]) -> {disconnect,T};
+parse_msg([<<"CONNECT">>|T]) -> {connect, {headers,parse_headers(T)}};
+parse_msg([<<"DISCONNECT">>|T]) -> {disconnect,{headers,parse_headers(T)}};
+parse_msg([<<"SEND">>|T]) -> {send,{headers,parse_headers(T)},{body,parse_body(T)}};
 parse_msg(Bin) -> {something_else,Bin}.
 
 drop_cr(Bin) when is_binary(Bin) -> binary:replace(Bin,<<?CR>>,<<>>,[global]);
 drop_cr(_Bin) -> <<>>.
 
-drop_lf(Bin) when is_binary(Bin) -> binary:replace(Bin,<<?LF>>,<<>>,[global]);
-drop_lf(_Bin) -> <<>>.
+parse_headers([]) -> [];
+parse_headers([H|_]) ->
+    BinList = binary:split(H,<<?LF>>,[global]),
+    parse_headers(BinList,[]).
 
-is_valid(Element) ->
-    case Element of
-        <<>> -> false;
-        <<?NULL>> -> false;
-        _ -> true
-    end.
-
-get_headers(BinList) -> get_headers(BinList,[]).
-
-get_headers([],Headers) -> Headers;
-get_headers([H|T],Headers) ->
+parse_headers([],Headers) -> to_headers(Headers);
+parse_headers([H|T],Headers) ->
     case H of
-        <<>> -> Headers;
-        Else -> get_headers(T,[Else|Headers])
+        <<>> -> to_headers(Headers);
+        <<?NULL>> -> to_headers(Headers);
+        Else -> parse_headers(T,[Else|Headers])
     end.
+
+parse_body([]) -> [];
+parse_body([H|_]) ->
+    [_H|Body] = binary:split(H,<<?LF,?LF>>,[global]),
+    [BinBody|_] = Body,
+    BinList = binary:split(BinBody,<<?LF>>,[global]),
+    parse_body(BinList,[]).
+
+parse_body([],Body) -> to_list(Body);
+parse_body([H|T],Body) ->
+    case H of
+        <<>> -> to_list(Body);
+        <<?NULL>> -> to_list(Body);
+        Else -> parse_body(T,[Else|Body])
+    end.
+
+get_valid_data(Bin) ->
+    [H|_] = binary:split(Bin,<<?NULL,?LF>>,[trim]),
+    H.
+
+to_list(BinList) -> lists:map(fun(X) -> binary:bin_to_list(X) end, lists:reverse(BinList)).
+
+to_headers(BinHeaders) -> to_headers(BinHeaders,[]).
+
+to_headers([],Headers) -> Headers;
+to_headers([H|T],Headers) ->
+    {BinK,BinV} = list_to_tuple(binary:split(H,<<":">>)),
+    to_headers(T,[{bin_to_list(BinK), bin_to_list(BinV)}|Headers]).
 
