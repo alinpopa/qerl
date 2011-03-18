a888cfff6ae3c58f74ff50a730a45a470f2c2589
alinpopa <alin.popa@gmail.com>
2011-03-18 18:04:30 +0200

    Add null checking functionality.

--- src/qerl_stomp_frame_parser.erl
+++ src/qerl_stomp_frame_parser.erl
@@ -2,21 +2,33 @@
 -behaviour(gen_fsm).
 
 -export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
--export([start_link/0]).
--export([new/3,parse/2]).
+-export([start_link/0,parse/2]).
+-export(['WAITING'/3]).
 
--record(state,{frames=[]}).
+-import(binary, [bin_to_list/1, match/2, replace/4, split/2, split/3]).
+
+-record(state,{frames=[], uncomplete_data = <<>>, is_null = false}).
 
 start_link() -> gen_fsm:start_link(?MODULE, [], []).
 
 parse(ParserPid,Data) ->
   gen_fsm:sync_send_event(ParserPid,{parse,Data}).
 
-new({parse,Data},_From,State) ->
+'WAITING'({parse,Data}, _From, State) ->
   ExistingFrames = State#state.frames,
-  {reply,{ok,ExistingFrames},new,#state{frames = [Data|ExistingFrames]}}.
+  UncompleteData = State#state.uncomplete_data,
+  io:format("Is uncomplete data: ~p~n",[is_null(UncompleteData)]),
+  io:format("Is first byte null: ~p~n",[is_first_byte_null(Data)]),
+  io:format("Existing byte null: ~p~n",[State#state.is_null]),
+  {reply, {waiting, ExistingFrames}, 'WAITING',
+    #state{
+            frames = [Data|ExistingFrames],
+            uncomplete_data = <<Data/binary,UncompleteData/binary>>,
+            is_null = is_null(Data)
+        }
+  }.
 
-init([]) -> {ok,new,#state{}}.
+init([]) -> {ok, 'WAITING', #state{}}.
 
 handle_event(_Event, _StateName, StateData) -> {stop, unimplemented, StateData}.
 handle_sync_event(_Event, _From, StateName, StateData) ->
@@ -25,4 +37,17 @@ handle_sync_event(_Event, _From, StateName, StateData) ->
 handle_info(_Info, _StateName, StateData) -> {stop, unimplemented, StateData}.
 terminate(_Reason, _StateName, _StateData) -> ok.
 code_change(_OldVsn, StateName, StateData, _Extra) -> {ok, StateName, StateData}.
+
+is_first_byte_null(<<>>) -> false;
+is_first_byte_null(UncompleteData) ->
+  case binary:first(UncompleteData) of
+    0 -> true;
+    _ -> false
+  end.
+
+is_null(UncompleteData) ->
+  case match(UncompleteData,<<0>>) of
+      nomatch -> false;
+      _ -> true
+    end.
 
