0be894add5e6dcbeacba6d919d82a24e011ed1b0
Alin Popa <alin.popa@gmail.com>
2011-03-20 08:45:06 +0200

    Add 'new' state to parse fsm.

--- src/qerl_stomp_frame_parser.erl
+++ src/qerl_stomp_frame_parser.erl
@@ -3,32 +3,39 @@
 
 -export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
 -export([start_link/0,parse/2]).
--export(['WAITING'/3]).
+-export(['NEW'/3,'WAITING'/3]).
 
 -import(binary, [bin_to_list/1, match/2, replace/4, split/2, split/3]).
 
--record(state,{frames=[], uncomplete_data = <<>>, is_null = false}).
+-record(state,{frames=[], uncomplete_data = <<>>}).
+
+-define(LF,10).
+-define(CR,13).
+-define(NULL,0).
 
 start_link() -> gen_fsm:start_link(?MODULE, [], []).
 
 parse(ParserPid,Data) ->
   gen_fsm:sync_send_event(ParserPid,{parse,Data}).
 
+'NEW'({parse,Data}, _From, State) ->
+  case drop(cr,Data) of
+    <<?LF>> -> {reply,{new,[]},'NEW',State};
+    _ ->
+      ExistingFrames = State#state.frames,
+      {reply,{waiting,ExistingFrames},'WAITING',#state{frames = [Data|ExistingFrames], uncomplete_data = drop(cr,Data)}}
+  end.
+
 'WAITING'({parse,Data}, _From, State) ->
   ExistingFrames = State#state.frames,
   UncompleteData = State#state.uncomplete_data,
-  io:format("Is uncomplete data: ~p~n",[is_null(UncompleteData)]),
-  io:format("Is first byte null: ~p~n",[is_first_byte_null(Data)]),
-  io:format("Existing byte null: ~p~n",[State#state.is_null]),
+  NoCrData = drop(cr,Data),
+  io:format("Uncomplete data: ~p~n",[UncompleteData]),
   {reply, {waiting, ExistingFrames}, 'WAITING',
-    #state{
-            frames = [Data|ExistingFrames],
-            uncomplete_data = <<Data/binary,UncompleteData/binary>>,
-            is_null = is_null(Data)
-        }
+    #state{frames = lists:reverse([Data|ExistingFrames]), uncomplete_data = <<UncompleteData/binary,NoCrData/binary>>}
   }.
 
-init([]) -> {ok, 'WAITING', #state{}}.
+init([]) -> {ok, 'NEW', #state{}}.
 
 handle_event(_Event, _StateName, StateData) -> {stop, unimplemented, StateData}.
 handle_sync_event(_Event, _From, StateName, StateData) ->
@@ -38,16 +45,12 @@ handle_info(_Info, _StateName, StateData) -> {stop, unimplemented, StateData}.
 terminate(_Reason, _StateName, _StateData) -> ok.
 code_change(_OldVsn, StateName, StateData, _Extra) -> {ok, StateName, StateData}.
 
-is_first_byte_null(<<>>) -> false;
-is_first_byte_null(UncompleteData) ->
-  case binary:first(UncompleteData) of
-    0 -> true;
-    _ -> false
-  end.
-
-is_null(UncompleteData) ->
-  case match(UncompleteData,<<0>>) of
-      nomatch -> false;
-      _ -> true
-    end.
+drop(_,<<>>) -> <<>>;
+drop(What,Bin) when is_binary(Bin) ->
+    case What of
+        cr -> replace(Bin,<<?CR>>,<<>>,[global]);
+        null -> replace(Bin,<<?NULL>>,<<>>,[global]);
+        _ -> Bin
+    end;
+drop(_What,_Bin) -> <<>>.
 
